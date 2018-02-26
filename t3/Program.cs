b6e79e434f1b97ce0b252111111d405fded8a2a8
pritch90 <pritch90@gmail.com>
2018-02-26 19:09:55 +0000

    initial project setup including chrome driver

---
+++ t3/Program.cs
@@ -0,0 +1,20 @@
+﻿using OpenQA.Selenium.Chrome;
+
+namespace t3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var driver = new ChromeDriver("Drivers"))
+            {
+                driver.Url = @"http://www.google.com";
+
+                var query = driver.FindElementByName("q");
+                
+                query.SendKeys("Cheese");
+                query.Submit();
+            }
+        }
+    }
+}

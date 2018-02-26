808a7b77d9d954b727c20bdc8e6fa413655773b7
pritch90 <pritch90@gmail.com>
2018-02-26 20:59:10 +0000

    Move runner to specific directory

---
+++ t3/t3.Runner/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace t3.Runner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+            
+            
+        }
+    }
+}
